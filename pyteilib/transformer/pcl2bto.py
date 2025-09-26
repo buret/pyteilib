9b869bdb5c43fe0fce89a139fd493bfadf81f88c
Celine Buret <celine.buret@univ-tlse2.fr>
2025-09-26 11:11:08 +0200

    Implement a new scenario

--- pyteilib/transformer/pcl2bto.py
+++ pyteilib/transformer/pcl2bto.py
@@ -4,8 +4,29 @@
 """! @package transformer
 """
 
-#from tei_p5 import
+from tei_p5 import projectDesc, catDesc, div, p
 from bto import BTO
+
+# Map genre from ParCoLab to BaTelÒc
+genre_mapping = {
+    'novel'  : 'roman',
+    'tale' : 'conte literari',
+    'memoirs' : 'memòris e cronicas',
+    'short story' : 'novèlas',
+    'essay' : 'ensag',
+    'poem' : 'poesia',
+    'theater' : 'teatre',
+    'song' : 'cançon',
+    'letter' : 'correspondéncia',
+    'speech' : 'discors',
+    'convention' : 'tractat',
+    'fairy tale' : 'conte de tradicion orala',
+    'scientific paper' : 'article scientific',
+    'newspaper article' : 'article de premsa',
+    'spoken' : 'tèxte oral',
+    '' : '',
+    None : None
+}
 
 def transform_pcl2bto(tei_pcl, xml_type='TEI'):
     """! @brief Transform a TEI instance type to another.
@@ -20,7 +41,158 @@ def transform_pcl2bto(tei_pcl, xml_type='TEI'):
     tei_bto = BTO()
     tei_bto.id = tei_pcl.id
 
-    #if xml_type == 'PCLv8':
-    #elif xml_type == 'PCLv9':
+    #### TEI header
+
+    ### fileDesc
+
+    ## titleStmt
+
+    # Set title in 2 different places
+    tei_bto.teiHeader.fileDesc.titleStmt.title = tei_pcl.teiHeader.fileDesc.titleStmt.title
+    tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.title[0].text = tei_pcl.teiHeader.fileDesc.titleStmt.title
+    # Set subtitle
+    tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.title[1].text = tei_pcl.teiHeader.fileDesc.titleStmt.subtitle
+    # Set author in 2 different places
+    if len(tei_pcl.teiHeader.fileDesc.titleStmt.author) > 0:
+        tei_bto.teiHeader.fileDesc.titleStmt.author = tei_pcl.teiHeader.fileDesc.titleStmt.author[0].name.surname + ' ' + tei_pcl.teiHeader.fileDesc.titleStmt.author[0].name.forename
+    tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[1].surname = tei_pcl.teiHeader.fileDesc.titleStmt.author[0].name.surname
+    tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[1].forename = tei_pcl.teiHeader.fileDesc.titleStmt.author[0].name.forename
+    if xml_type == 'PCLv9':
+        tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[0].datebirth = tei_pcl.teiHeader.fileDesc.titleStmt.author[0].birth
+        tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[0].datedead = tei_pcl.teiHeader.fileDesc.titleStmt.author[0].death
+    # Leave editor empty
+    # Compute principal
+    if len(tei_pcl.teiHeader.fileDesc.titleStmt.collectionStmt.respStmt) > 0:
+        tei_bto.teiHeader.fileDesc.titleStmt.principal = tei_pcl.teiHeader.fileDesc.titleStmt.collectionStmt.respStmt[0].name.forename + ' ' + tei_pcl.teiHeader.fileDesc.titleStmt.collectionStmt.respStmt[0].name.surname
+    # Set 5 respStmt
+    set_translator = True
+    for item in tei_pcl.teiHeader.fileDesc.titleStmt.respStmt:
+        # Set 'balhaire fichièr sorga'
+        if item.resp == "publisher_provider":
+            tei_bto.teiHeader.fileDesc.titleStmt.respStmt[0].name = item.name
+        # Set 'portaire fichièr sorga'
+        elif item.resp == "project_provider":
+            tei_bto.teiHeader.fileDesc.titleStmt.respStmt[1].name = item.name
+        # Set 'donadas pel header'
+        elif item.resp == "teihdr_creator":
+            tei_bto.teiHeader.fileDesc.titleStmt.respStmt[2].name = item.name
+        # Set 'pretractament e balisatge del tèxte segon la TEI P5'
+        elif item.resp == "teibdy_creator":
+            tei_bto.teiHeader.fileDesc.titleStmt.respStmt[3].name = item.name
+        # Set first translator
+        elif item.resp == "translator" and set_translator:
+            tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[0].surname = item.name.surname
+            tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[0].forename = item.name.forename
+            tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[0].datebirth = item.birth
+            tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[0].datedead = item.death
+            set_translator = False
+
+    ## editionStmt
+    if xml_type == 'PCLv9':
+        # Set edition date as last change
+        if len(tei_pcl.revisionDesc.listcChange.change) > 0:
+            tei_bto.teiHeader.fileDesc.editionStmt.edition.date = tei_pcl.teiHeader.revisionDesc.listcChange.change[0].when
+
+    ## extent
+    if tei_pcl.teiHeader.fileDesc.extent is not None:
+        tei_bto.teiHeader.fileDesc.extent = tei_pcl.teiHeader.fileDesc.extent.measure.quantity + ' ' + tei_pcl.teiHeader.fileDesc.extent.measure.unit
+
+    ## publicationStmt
+
+    # Compute distributor's name
+    if len(tei_pcl.teiHeader.fileDesc.titleStmt.collectionStmt.respStmt) > 1:
+        tei_bto.teiHeader.fileDesc.publicationStmt.distributor.name = tei_pcl.teiHeader.fileDesc.titleStmt.collectionStmt.respStmt[1].name.surname + '-' + tei_pcl.teiHeader.fileDesc.titleStmt.collectionStmt.respStmt[1].name.forename
+    # Set availability
+    tei_bto.teiHeader.fileDesc.publicationStmt.availability.status = tei_pcl.teiHeader.fileDesc.publicationStmt.availability.licence.text
+    tei_bto.teiHeader.fileDesc.publicationStmt.availability.p = tei_pcl.teiHeader.fileDesc.publicationStmt.availability.licence.p
+
+    ## sourceDesc
+
+    # Hard-code bibliography structure lang
+    tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.lang = "fr"
+    # Set bibliography structure monography
+    tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.date = tei_pcl.teiHeader.fileDesc.publicationStmt.date
+    tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.publisher = tei_pcl.teiHeader.fileDesc.publicationStmt.publisher
+    # Split edition into series and idno
+    edition_list = tei_pcl.teiHeader.fileDesc.editionStmt.edition.text.split()
+    if len(edition_list) > 1:
+        tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.series = edition_list[0]
+        tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.idno[1].text = edition_list[1]
+    elif len(edition_list) == 1:
+        try:
+            # If this a an integer, set idno
+            tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.idno[1].text = int(edition_list[0])
+        except ValueError:
+            # If this is a string, set series statement
+            tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.seriesStmt = edition_list[0]
+    tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.pubplace = tei_pcl.teiHeader.fileDesc.publicationStmt.pubPlace
+    tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.idno[0].text = tei_pcl.teiHeader.fileDesc.publicationStmt.idno
+
+    ### encodingDesc
+
+    ## projectDesc
+    if tei_pcl.teiHeader.encodingDesc.projectDesc is not None:
+        tei_bto.teiHeader.encodingDesc.projectDesc = projectDesc()
+        for item in tei_pcl.teiHeader.encodingDesc.projectDesc.p:
+            tei_bto.teiHeader.encodingDesc.projectDesc.p.append(p(text=item.text))
+
+    ## classDecl
+    # Hard-code taxonomy bibliography
+    tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.bibl = "Frantext"
+    # Set dialect
+    if xml_type == 'PCLv8':
+        if tei_pcl.teiHeader.profileDesc.category.type == "dialecte" and len(tei_pcl.teiHeader.profileDesc.category.catDesc) > 0:
+            tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[0].catDesc[0].text = tei_pcl.teiHeader.profileDesc.category.catDesc[0].text
+    elif xml_type == 'PCLv9':
+        tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[0].catDesc[0].text = tei_pcl.teiHeader.profileDesc.langUsage.language.variant
+    if xml_type == 'PCLv9':
+        # Set script
+        tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[1].catDesc[0].text = tei_pcl.teiHeader.profileDesc.langUsage.language.script
+    # Map genre
+    genre_bto = None
+    index = 0
+    for item in tei_pcl.teiHeader.profileDesc.textDesc.genre:
+        if item.lang == "en":
+            if item.type in genre_mapping:
+                genre_bto = genre_mapping[item.type]
+            else:
+                genre_bto = "autre"
+            if len(tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[2].catDesc) > index :
+                tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[2].catDesc[index].text = genre_bto
+                index += 1
+    if xml_type == 'PCLv9':
+        # Set corpus
+        for item in tei_pcl.teiHeader.profileDesc.textClass.keywords.term:
+            tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[3].catDesc.append(catDesc(text=item.text))
+
+    ### profileDesc
+
+    ## creation
+    # Set date
+    tei_bto.teiHeader.profileDesc.creation.date = tei_pcl.teiHeader.profileDesc.creation.date
+
+    ## langUsage
+    # Set language
+    tei_bto.teiHeader.profileDesc.langUsage.language.ident = tei_pcl.teiHeader.profileDesc.langUsage.language.ident
+    if tei_pcl.teiHeader.profileDesc.langUsage.language.ident == "oc":
+        tei_bto.teiHeader.profileDesc.langUsage.language.text = "Occitan"
+
+    #### TEI text
+
+    ## Clone body divisions and sub-divisions
+    for div_pcl in tei_pcl.text.body.div:
+        if (div_pcl.text is not None and div_pcl.text != '') or (div_pcl.head.seg is not None and div_pcl.head.seg != '') or len(div_pcl.p) > 0:
+            div_bto = div(text=div_pcl.text)
+            div_bto.head.text = div_pcl.head.seg
+            for p_pcl in div_pcl.p:
+                p_bto = p(text=p_pcl.text)
+                for s_pcl in p_pcl.s:
+                    if s_pcl.text is not None and s_pcl.text != '':
+                        if p_bto.text is None or p_bto.text == '':
+                            p_bto.text = s_pcl.text
+                        else:
+                            p_bto.text += ' ' + s_pcl.text
+                div_bto.p.append(p_bto)
+            tei_bto.text.body.div.append(div_bto)
 
     return tei_bto
--- test/scenario2.py
+++ test/scenario2.py
@@ -128,5 +128,96 @@ tei_bto = pyteilib.transform(tei_pclv8, from_type='PCLv8', to_type='BTO')
 assert tei_bto.__class__.__name__ == "BTO", tei_bto.__class__.__name__
 assert tei_bto.id == "test_2", tei_bto.id
 
+# Test teiHeader
+assert tei_bto.teiHeader.fileDesc.titleStmt.title == "Title", tei_bto.teiHeader.fileDesc.titleStmt.title
+assert tei_bto.teiHeader.fileDesc.titleStmt.author == "Lastname Firstname", tei_bto.teiHeader.fileDesc.titleStmt.author
+assert tei_bto.teiHeader.fileDesc.titleStmt.editor == None, tei_bto.teiHeader.fileDesc.titleStmt.editor
+assert tei_bto.teiHeader.fileDesc.titleStmt.principal == "Firstname Lastname", tei_bto.teiHeader.fileDesc.titleStmt.principal
+assert tei_bto.teiHeader.fileDesc.titleStmt.respStmt[0].resp == "balhaire fichièr sorga", tei_bto.teiHeader.fileDesc.titleStmt.respStmt[0].resp
+assert tei_bto.teiHeader.fileDesc.titleStmt.respStmt[0].name == None, tei_bto.teiHeader.fileDesc.titleStmt.respStmt[0].name
+assert tei_bto.teiHeader.fileDesc.titleStmt.respStmt[1].resp == "portaire fichièr sorga", tei_bto.teiHeader.fileDesc.titleStmt.respStmt[1].resp
+assert tei_bto.teiHeader.fileDesc.titleStmt.respStmt[1].name == None, tei_bto.teiHeader.fileDesc.titleStmt.respStmt[1].name
+assert tei_bto.teiHeader.fileDesc.titleStmt.respStmt[2].resp == "donadas pel header", tei_bto.teiHeader.fileDesc.titleStmt.respStmt[2].resp
+assert tei_bto.teiHeader.fileDesc.titleStmt.respStmt[2].name == None, tei_bto.teiHeader.fileDesc.titleStmt.respStmt[2].name
+assert tei_bto.teiHeader.fileDesc.titleStmt.respStmt[3].resp == "pretractament e balisatge del tèxte segon la TEI P5", tei_bto.teiHeader.fileDesc.titleStmt.respStmt[3].resp
+assert tei_bto.teiHeader.fileDesc.titleStmt.respStmt[3].name == None, tei_bto.teiHeader.fileDesc.titleStmt.respStmt[3].name
+assert tei_bto.teiHeader.fileDesc.editionStmt.edition.date == None, tei_bto.teiHeader.fileDesc.editionStmt.edition.date
+assert tei_bto.teiHeader.fileDesc.extent == "114 words", tei_bto.teiHeader.fileDesc.extent
+assert tei_bto.teiHeader.fileDesc.publicationStmt.distributor.name == "CLLE-LL-OCRE", tei_bto.teiHeader.fileDesc.publicationStmt.distributor.name
+assert tei_bto.teiHeader.fileDesc.publicationStmt.distributor.address == None, tei_bto.teiHeader.fileDesc.publicationStmt.distributor.address
+assert tei_bto.teiHeader.fileDesc.publicationStmt.availability.status == "restricted", tei_bto.teiHeader.fileDesc.publicationStmt.availability.status
+assert tei_bto.teiHeader.fileDesc.publicationStmt.availability.p.text == "à utiliser dans un cadre de recherche ou d'enseignement après accord du dépositaire des droits", tei_bto.teiHeader.fileDesc.publicationStmt.availability.p.text
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.lang == "fr", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.lang
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[0].id == "traducteur", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[0].id
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[0].surname == "Lastname", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[0].surname
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[0].forename == "Firstname", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[0].forename
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[0].datebirth == None, tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[0].datebirth
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[0].datedead == None, tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[0].datedead
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[1].id == "auteurOriginal", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[1].id
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[1].surname == "Lastname", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[1].surname
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[1].forename == "Firstname", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[1].forename
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[1].datebirth == None, tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[1].datebirth
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[1].datedead == None, tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.author[1].datedead
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.title[0].type == None, tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.title[0].type
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.title[0].text == "Title", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.title[0].text
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.title[1].type == "parallel", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.title[1].type
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.title[1].text == "Subtitle", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.title[1].text
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.date == "Date", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.date
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.publisher == "Publisher", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.publisher
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.series == "Series", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.series
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.pubplace == "Place", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.pubplace
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.idno[0].type == "ISBN", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.idno[0].type
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.idno[0].text == None, tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.idno[0].text
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.idno[1].type == "numero edition", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.idno[1].type
+assert tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.idno[1].text == "123", tei_bto.teiHeader.fileDesc.sourceDesc.biblStruct.monogr.imprint.idno[1].text
+assert tei_bto.teiHeader.encodingDesc.projectDesc.p[0].text == "BaTelÒc : Basa de Tèxtes en Lenga Occitana", tei_bto.teiHeader.encodingDesc.projectDesc.p[0].text
+assert tei_bto.teiHeader.encodingDesc.projectDesc.p[1].text == "Encodatge de tèxtes occitans segon la nòrma de la Text Encoding Initiative, TEIP5", tei_bto.teiHeader.encodingDesc.projectDesc.p[1].text
+assert tei_bto.teiHeader.encodingDesc.projectDesc.p[2].text == "http://www.tei-c.org/release/doc/tei-p5-doc", tei_bto.teiHeader.encodingDesc.projectDesc.p[2].text
+assert tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.bibl == "Frantext", tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.bibl
+assert tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[0].type == "dialecte", tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[0].type
+assert tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[0].catDesc[0].text == "Lengadocian", tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[0].catDesc[0].text
+assert tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[1].type == "graphie", tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[1].type
+assert tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[1].catDesc[0].text == None, tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[1].catDesc[0].text
+assert tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[2].type == "genre", tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[2].type
+assert tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[2].catDesc[0].type == "principal", tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[2].catDesc[0].type
+assert tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[2].catDesc[0].text == "ensag", tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[2].catDesc[0].text
+assert tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[2].catDesc[1].type == "sub", tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[2].catDesc[1].type
+assert tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[2].catDesc[1].text == None, tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[2].catDesc[1].text
+assert tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[3].type == "corpus", tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[3].type
+assert len(tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[3].catDesc) == 0, len(tei_bto.teiHeader.encodingDesc.classDecl.taxonomy.category[3].catDesc)
+assert tei_bto.teiHeader.profileDesc.creation.date == "2003", tei_bto.teiHeader.profileDesc.creation.date
+assert tei_bto.teiHeader.profileDesc.langUsage.language.ident == "oc", tei_bto.teiHeader.profileDesc.langUsage.language.ident
+assert tei_bto.teiHeader.profileDesc.langUsage.language.text == "Occitan", tei_bto.teiHeader.profileDesc.langUsage.language.text
+
+# Test text body
+assert tei_bto.text.body.div[0].__class__.__name__ == "div", tei_bto.text.body.div[0].__class__.__name__
+assert tei_bto.text.body.div[0].text == '', tei_bto.text.body.div[0].text
+assert tei_bto.text.body.div[0].head.text == None, tei_bto.text.body.div[0].head.text
+assert tei_bto.text.body.div[0].p[0].text == "This is a quote.", tei_bto.text.body.div[0].p[0].text
+assert tei_bto.text.body.div[0].p[1].text == "Firstname LASTNAME.", tei_bto.text.body.div[0].p[1].text
+assert tei_bto.text.body.div[1].__class__.__name__ == "div", tei_bto.text.body.div[1].__class__.__name__
+assert tei_bto.text.body.div[1].text == '', tei_bto.text.body.div[1].text
+assert tei_bto.text.body.div[1].head.text == None, tei_bto.text.body.div[1].head.text
+assert tei_bto.text.body.div[1].p[0].text == "Aquò es un exemple.", tei_bto.text.body.div[1].p[0].text
+assert tei_bto.text.body.div[1].p[1].text == "Amb qualquas frasas.", tei_bto.text.body.div[1].p[1].text
+assert tei_bto.text.body.div[1].p[2].text == "E de partidas en italicas. De questions? O encara: d'exclamacions! Plan.", tei_bto.text.body.div[1].p[2].text
+assert tei_bto.text.body.div[1].p[3].text == "De verguetas : \"100% des tests doivent passer\". Totjorn. \"Alara ?\"", tei_bto.text.body.div[1].p[3].text
+assert tei_bto.text.body.div[1].p[4].text == "Ça, c'est une phrase qui commence par un caractère spécial. Ô oui! Ò aquesta tanben.", tei_bto.text.body.div[1].p[4].text
+assert tei_bto.text.body.div[2].__class__.__name__ == "div", tei_bto.text.body.div[2].__class__.__name__
+assert tei_bto.text.body.div[2].text == '', tei_bto.text.body.div[2].text
+assert tei_bto.text.body.div[2].head.text == "Titre du chapitre", tei_bto.text.body.div[2].head.text
+assert tei_bto.text.body.div[2].p[0].text == "Maintenant testons les abbréviations comme F.T.P ou F.F.I. par exemple. Ou St. (St ?) Mr. et Ste Mme.", tei_bto.text.body.div[2].p[0].text
+assert tei_bto.text.body.div[2].p[1].text == "Ainsi que les différentes ponctuations,  comme ; et encore : ainsi de suite... Voilà.", tei_bto.text.body.div[2].p[1].text
+assert tei_bto.text.body.div[2].p[2].text == "Nous devons aussi tester les dialogues. Quelqu'un dit : - Que fait-on ? Doit-on continuer ?", tei_bto.text.body.div[2].p[2].text
+assert tei_bto.text.body.div[2].p[3].text == "- Je ne sais pas... Qu'en penses-tu ?", tei_bto.text.body.div[2].p[3].text
+assert tei_bto.text.body.div[3].__class__.__name__ == "div", tei_bto.text.body.div[3].__class__.__name__
+assert tei_bto.text.body.div[3].text == '', tei_bto.text.body.div[3].text
+assert tei_bto.text.body.div[3].head.text == None, tei_bto.text.body.div[3].head.text
+assert tei_bto.text.body.div[3].p[0].text == "C'est la fin. On termine avec une signature.", tei_bto.text.body.div[3].p[0].text
+
+## Simply export it to BaTelÒc XML
+pyteilib.tei_write(tei_bto, OUTPUT_DIR + "example2_bto.xml", xml_type='BTO')
+assert tei_bto.get_filename() == OUTPUT_DIR + "example2_bto.xml", tei_bto.get_filename()
+
 ## Release created objects
 del tei_pclv8, tei_bto
